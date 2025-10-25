623fc8fddf5dafdf1617d93e55a6205bfba4527d
Olha Brekhuntsova <olga.pistryak@gmail.com>
2025-10-25 18:10:45 +0200

    add 'the same color' check at task 5

--- js/task-5.js
+++ js/task-5.js
@@ -4,12 +4,22 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+function rgbToHex(rgb) {
+  const [r, g, b] = rgb.slice(4, -1).split(",");
+  const toHex = (n) => n.toString(16).padStart(2, "0");
+  return `#${toHex(+r)}${toHex(+g)}${toHex(+b)}`;
+}
+
 const bodyElem = document.querySelector("body");
 const colorBtnElem = document.querySelector("button.change-color");
 const colorNameElem = document.querySelector("span.color");
 
 colorBtnElem.addEventListener("click", () => {
-  const color = getRandomHexColor();
-  bodyElem.style.backgroundColor = color;
-  colorNameElem.textContent = color;
+  const bodyColor = window.getComputedStyle(bodyElem).backgroundColor;
+  let newColor = getRandomHexColor();
+  while (rgbToHex(bodyColor) === newColor) {
+    newColor = getRandomHexColor();
+  }
+  bodyElem.style.backgroundColor = newColor;
+  colorNameElem.textContent = newColor;
 });
